41ed1ab984f639450bc06739d6d56f6d261f18d5
Vilma Cobbler <vcobbler@ev-a1.kzn.21-school.ru>
2021-04-19 22:15:56 +0300

    update, norme

--- ft_calloc.c
+++ ft_calloc.c
@@ -2,16 +2,13 @@
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
-	void	*void_alloc_mem;
-	char	*char_alloc_mem;
+	void	*alloc_mem;
 	size_t	total_bytes;
 
 	if (nmemb * size > (size_t)0 - 1 || !nmemb || !size)
 		return (NULL);
 	total_bytes = nmemb * size;
-	void_alloc_mem = malloc(total_bytes);
-	char_alloc_mem = (char *)void_alloc_mem;
-	while (total_bytes--)
-		char_alloc_mem[total_bytes] = 0;
-	return (void_alloc_mem);
+	alloc_mem = malloc(total_bytes);
+	ft_bzero(alloc_mem, total_bytes);
+	return (alloc_mem);
 }
---
+++ ft_split.c
@@ -0,0 +1,60 @@
+#include "libft.h"
+
+static size_t	ft_wrdcnt(char const *s, char c)
+{
+	size_t	count;
+	char	in_word_flag;
+
+	count = 0;
+	in_word_flag = 0;
+	while (*s == c)
+		s++;
+	if (*s)
+		count++;
+	while (*s)
+	{
+		if (*s == c && *(s + 1) != c && *(s + 1) && !in_word_flag)
+		{
+			in_word_flag++;
+			count++;
+		}
+		else if (*(s + 1) == c && in_word_flag)
+			in_word_flag--;
+		s++;
+	}
+	return (count);
+}
+
+static void	ft_getword(const char **start, const char **end, char c)
+{
+	while (**start == c)
+		(*start)++;
+	*end = *start + 1;
+	while (**end != c)
+		(*end)++;
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char		**result;
+	size_t		result_size;
+	size_t		iter_1;
+	const char	*start;
+	const char	*end;
+
+	if (!s)
+		return (NULL);
+	result = (char **)ft_calloc((
+				result_size = ft_wrdcnt(s, c)) + 1, sizeof(char *));
+	if (!result)
+		return (NULL);
+	start = s;
+	iter_1 = 0;
+	while (iter_1 < result_size)
+	{
+		ft_getword(&start, &end, c);
+		result[iter_1++] = ft_strldup(start, end - start);
+		start = end + 1;
+	}
+	return (result);
+}
