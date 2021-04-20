b60a392f26d57a884890dbda7f2f4bda87ee7b45
Vilma Cobbler <vcobbler@ev-n2.kzn.21-school.ru>
2021-04-20 22:13:25 +0300

    norme

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: vcobbler <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/16 16:09:11 by vcobbler          #+#    #+#             */
-/*   Updated: 2021/04/20 16:18:38 by vcobbler         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
@@ -21,6 +9,6 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 		return (NULL);
 	destination = (unsigned char *)dst;
 	while (n--)
-		*destination++ = *source++;
+		destination[n] = source[n];
 	return (dst);
 }
--- ft_memset.c
+++ ft_memset.c
@@ -1,25 +1,13 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: vcobbler <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/16 14:26:53 by vcobbler          #+#    #+#             */
-/*   Updated: 2021/04/16 17:28:03 by vcobbler         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
 void	*ft_memset(void *b, int c, size_t len)
 {
-	unsigned char	*ptr;
+	unsigned char	*string;
 	unsigned char	value;
 
-	ptr = (unsigned char *)b;
+	string = (unsigned char *)b;
 	value = (unsigned char)c;
 	while (len--)
-		*ptr++ = value;
+		string[len] = value;
 	return (b);
 }
