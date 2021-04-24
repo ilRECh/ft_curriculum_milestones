f0b5dc22269b71a121b1def39b1f5e70ed017b8a
Vilma Cobbler <vcobbler@ev-b2.kzn.21-school.ru>
2021-04-24 20:58:57 +0300

    clearing repo

---
+++ cmake-build-debug/CMakeFiles/ft_printf_.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ft_printf_.dir/1234.c.o"
+  "CMakeFiles/ft_printf_.dir/main.c.o"
+  "ft_printf_"
+  "ft_printf_.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ft_printf_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
