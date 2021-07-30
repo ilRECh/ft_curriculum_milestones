4084c05600abf47a3663eb3095115807870fde84
Clarissa Samuro <csamuro@er-a5.kzn.21-school.ru>
2021-07-31 00:44:57 +0300

    add gnl, include lib for printf, add gitignore

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,6 +1,6 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
-
-
+# include <stdio.h>
+# include "libft.h"
 
 #endif
--- main.c
+++ main.c
@@ -2,5 +2,13 @@
 
 int	main(int argc, char **argv, char **env)
 {
+	//* START TEST
+	char	*line;
+	while (get_next_line(0, &line) > 0)
+	{
+		printf("%s\n", line);
+		free(line);
+	}
+	//* END TEST
 	return (0);
 }
