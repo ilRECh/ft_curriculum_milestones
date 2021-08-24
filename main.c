41b60c49187c041582fcadb49bdf3aca9c445588
Clarissa Samuro <csamuro@er-a4.kzn.21-school.ru>
2021-08-24 19:26:00 +0300

    beta_020.05

--- main.c
+++ main.c
@@ -32,7 +32,6 @@ void	ctrl_c(int signum, siginfo_t *siginfo, void *code)
 	printf("\n" RED "super " CYAN "shell " RESET "$> ");
 	// execve("minishell", NULL, g_param->env);
 }
-// control + c; end
 
 int	main(int argc, char **argv, char **env)
 {
@@ -41,11 +40,16 @@ int	main(int argc, char **argv, char **env)
 	(void)env;
 // control + c
 	struct sigaction	control_c;
+	// struct sigaction	control_d;
 
 	ft_memset(&control_c, 0, sizeof(control_c));
+	// ft_memset(&control_d, 0, sizeof(control_d));
 	control_c.sa_sigaction = ctrl_c;
+	// control_d.sa_sigaction = ctrl_d;
 	control_c.sa_flags = SA_SIGINFO;
+	// control_d.sa_flags = SA_SIGINFO;
 	sigaction(SIGINT, &control_c, NULL);
+	// sigaction(????, &control_d, NULL);
 // control + c; end
 
 	g_param = ft_calloc(1, sizeof(t_param));
@@ -60,6 +64,8 @@ int	main(int argc, char **argv, char **env)
 	while (line)
 	{
 		line = readline(RED "super " CYAN "shell " RESET "$> ");
+		if (!line)
+			continue ;
 		add_history(line);
 		list_of_parses = get_command_line(&line);
 		// exec
