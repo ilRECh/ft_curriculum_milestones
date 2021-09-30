bf234e7015447de7af0e8b05625e362554a25723
ilRECh <ildar.rch@gmail.com>
2021-09-30 09:49:06 +0300

    working on parse. creating map. checking full map

--- src/parse/setup_map/make_map_from_lst.c
+++ src/parse/setup_map/make_map_from_lst.c
@@ -6,15 +6,21 @@
 /*   By: name <name@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 09:43:57 by name              #+#    #+#             */
-/*   Updated: 2021/09/30 09:44:07 by name             ###   ########.fr       */
+/*   Updated: 2021/09/30 09:48:47 by name             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+static bool	check_for_empty_lines(t_all *all, t_list *lst)
+{
+	
+}
+
 bool	make_map_from_lst(t_all *all, t_list *lst)
 {
-	(void)all;
-	(void)lst;
+	if (check_for_empty_lines(all, lst))
+		return ((all->err
+			= ft_strdup("map contains empty lines. Unforgivable.")), true);
 	return (false);
 }
