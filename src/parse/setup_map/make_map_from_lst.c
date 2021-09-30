dbae83975fe906bfbc19ab725ad8667437a21a01
ilRECh <ildar.rch@gmail.com>
2021-09-30 12:24:01 +0300

    working on parse. creating map. checking full map

--- src/parse/setup_map/make_map_from_lst.c
+++ src/parse/setup_map/make_map_from_lst.c
@@ -34,11 +34,62 @@ static bool	check_for_empty_lines(t_list *lst)
 	return (false);
 }
 
+static inline void	find_height_length(t_list *lst, int *height, int *length)
+{
+	int tmp;
+
+	*height = 0;
+	*length = 0;
+	lst->Dcur = lst->Dstart;
+	while (lst->Dcur)
+	{
+		*height += 1;
+		tmp = ft_strlen(lst->Dcur->content);
+		if (tmp > *length)
+			*length = tmp;
+		lst->Dcur = lst->Dcur->next;
+	}
+}
+
+static inline bool	make_new_map(t_all *all, int height, int length)
+{
+	int	tmp;
+
+	all->map = ft_calloc(height + 2, sizeof(char *));
+	if (!all->map)
+		return (true);
+	tmp = -1;
+	while (++tmp < height + 2)
+	{
+		all->map[tmp] = malloc((length + 3) * sizeof(char));
+		if (!all->map[tmp])
+			return (true);
+		all->map[tmp][length + 2] = 0;
+		ft_memset(all->map[tmp], ' ', length + 2);
+	}
+#ifdef DEBUG
+{
+	for (int i = 0; i < height + 2; i++)
+	{
+		printf("new_map[%3d]: |%s| ,len: %ld\n", i, all->map[i], ft_strlen(all->map[i]));
+	}
+}
+#endif
+	return (false);
+}
+
 bool	make_map_from_lst(t_all *all, t_list *lst)
 {
+	int	height;
+	int	length;
+
 	if (check_for_empty_lines(lst))
 		return ((all->err
 			= ft_strdup("map contains empty lines."
 				" Unforgivable. Shame on you.")), true);
+	find_height_length(lst, &height, &length);
+	if (make_new_map(all, height, length))
+		return ((all->err
+			= ft_strdup("no space left")), true);
 	return (false);
 }
