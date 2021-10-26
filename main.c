415b8bf3121e6242d9ad402940730225d1c95060
3Danger <3Danger@mail.ru>
2021-10-26 13:47:45 +0300

    before mouse code_

--- main.c
+++ main.c
@@ -45,10 +45,9 @@ int	cub3d(char *map)
 		fall(&all);
 		return (1);
 	}
-	all.screen_size = pnt_set(1280, 1280);
+	all.screen_size = pnt_s(640);
 	game(&all);
 	mlx_loop(all.win);
-	// while (true);
 	return (0);
 }
 
